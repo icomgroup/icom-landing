a703379c883ed4b5bfb4a5c1e73857b7ae62e19b
ICOM <105383896+icomgroup@users.noreply.github.com>
2022-07-04 22:03:51 +0300

    Update index.js

--- src/components/index.js
+++ src/components/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Carousel from "react-elastic-carousel";
 import Item from "./Item";
-import "./styles.css";
 import "../style.css";
 
 
